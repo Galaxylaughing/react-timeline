58ebd334a6eb2808cc038cdbdde428944a642ee7
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-11-20 14:55:51 -0800

    added TimelineEvent

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
+import TimelineEvent from './components/TimelineEvent';
 import Timeline from './components/Timeline';
 
 function App() {
-  console.log(timelineData);
+  console.log(timelineData.events[0]);
 
   // Customize the code below
   return (
@@ -14,6 +15,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent post={timelineData.events[0]} />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return (
+    <article>
+      <p>{props.post.person}</p>
+      <p>{props.post.status}</p>
+      <Timestamp time={props.post.timestamp} />
+    </article>
+  );
 }
 
 export default TimelineEvent;
